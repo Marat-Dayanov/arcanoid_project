57226130ccbfc99c55ae4fdf0845db87aceb44b3
ученик <dmitta>
2020-12-28 17:49:42 +0500

    add busters

---
+++ Busters.py
@@ -0,0 +1,23 @@
+from random import randrange
+
+import pygame
+from config import HEIGHT, WIDTH
+
+
+class Buster(pygame.sprite.Sprite):
+    def __init__(self, radius, x, y, all_sprites, player):
+        super().__init__(all_sprites)
+        self.radius = radius
+        self.image = pygame.Surface((2 * radius, 2 * radius),
+                                    pygame.SRCALPHA, 32)
+        pygame.draw.circle(self.image, pygame.Color("blue"),
+                           (radius, radius), radius)
+        self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
+        self.vy = 2
+        self.player = player
+
+    def update(self):
+        if pygame.sprite.collide_rect(self, self.player) or self.rect.y > HEIGHT:
+            for group in self.groups():
+                group.remove(self)
+        self.rect = self.rect.move(0, self.vy)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from ball import Ball
 from Player import *
 from block import *
 from levels import *
+from Busters import *
 
 
 class Game:
@@ -25,6 +26,7 @@ class Game:
 
         self.player = Player(self.all_sprites)
         self.balls = Ball(10, 20, 100, self.all_sprites, self.player, self.blocks)
+        self.buster = Buster(10, 10, 100, self.all_sprites, self.player)
 
 
 if __name__ == '__main__':
