a08df7e866926372d5572b1fb3e38f07fe43e57f
dmitr <ip\jn>
2021-01-13 19:51:53 +0500

    add star image

--- menu.py
+++ menu.py
@@ -1,5 +1,21 @@
 import sys
 import pygame
+from Utiles import *
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    return image
 
 
 class Menu:
@@ -15,15 +31,19 @@ class Menu:
 
     def render(self):
         self.game.screen.blit(self.img, (0, 0))
-        for i, item in enumerate(self.punkts):
-            if item.is_active:
-                text = self.font.render(item.name, True, item.active_color)
-            else:
-                text = self.font.render(item.name, True, item.base_color)
-            x = 180
-            y = i * 40 + 100
-            self.game.screen.blit(text, (x, y))
-
+        with open('completed_levels.txt', 'r') as file:
+            COMPLETED_LEVELS = file.readlines()
+            COMPLETED_LEVELS = [int(i) for i in COMPLETED_LEVELS]
+            for i, item in enumerate(self.punkts):
+                x = 180
+                y = i * 40 + 100
+                if COMPLETED_LEVELS[i]:
+                    self.game.screen.blit(MenuItem.star_image, (x, y))
+                if item.is_active:
+                    text = self.font.render(item.name, True, item.active_color)
+                else:
+                    text = self.font.render(item.name, True, item.base_color)
+                self.game.screen.blit(text, (x, y))
 
     def menu(self):
         done = True
@@ -58,9 +78,23 @@ class Menu:
             self.game.screen.blit(self.game.screen, (0, 0))
             pygame.display.flip()
 
+    def complete_level(self):
+        with open('completed_levels.txt', 'r') as file:
+            COMPLETED_LEVELS = file.readlines()
+            COMPLETED_LEVELS = [int(i) for i in COMPLETED_LEVELS]
+            for i, punkt in enumerate(self.punkts):
+                if punkt.is_active:
+                    COMPLETED_LEVELS[i] = 1
+
+        with open('completed_levels.txt', 'w') as file:
+            COMPLETED_LEVELS = [str(i) for i in COMPLETED_LEVELS]
+            file.write('\n'.join(COMPLETED_LEVELS))
+
 
 class MenuItem:
     game = None
+    star_image = load_image('star.jpg')
+    star_image_complete = load_image('star.jpg')
 
     def __init__(self, name, base_color, active_color, level):
         self.name = name
